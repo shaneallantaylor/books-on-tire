e9b9ced1d561943cd22f92eb27bb5c98b73c2e5d
Shane Taylor <shaneallantaylor@gmail.com>
2022-09-04 17:36:31 -0700

    This wont work

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { globalCss } from '@stitches/react';
+import { globalCss } from '../../stitches.config';
 
 function getRandomEmoji() {
   const possibleEmojis = ['👋🏻', '🫠', '🦜', '🎉', '💎', '📔', '🍉'];
